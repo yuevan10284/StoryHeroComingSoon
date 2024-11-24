c5855276f7eac3cd3fe09b2457296be791ec3370
Evan Yu <92699280+yuevan10284@users.noreply.github.com>
2024-11-23 19:23:11 -0800

    Update coming-soon.tsx

--- components/coming-soon.tsx
+++ components/coming-soon.tsx
@@ -20,7 +20,7 @@ export function ComingSoon() {
         <div className="mb-12 relative">
           <div className="absolute inset-0 blur-2xl bg-red-500/40"></div>
           <Image
-            src="/images/logo.png"
+            src="/images/Storyherologo.png"
             alt="StoryHero Logo"
             width={300}
             height={300}
